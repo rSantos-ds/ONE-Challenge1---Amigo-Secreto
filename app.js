186b0318f103b82d4c4d370ac017cdb43ef4763d
Rafael Santos <rafa.dss@outlook.com>
2025-01-17 19:37:45 -0300

    2 - Atualizar lista de nomes na tela

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+// Declaração do array para armazenar os nomes
 let listaNomes = [];
 
+// Função para adicionar nomes no array
 function adicionarAmigo() {
-  nome = document.querySelector("#amigo");
+  let campoNome = document.querySelector("#amigo");
+  let nome = campoNome.value.toUpperCase();
 
-  if (nome.value == "") {
+  //   Validação se o campo esta vazio e se o nome já consta na lista
+  if (nome == "") {
     alert("Campo vazio, por favor preencha com um nome ou valor!");
-  } else if (listaNomes.includes(nome.value)) {
-    alert(
-      "Nome ja foi inserido anteriormente, por favor insira apenas valores unicos"
-    );
+  } else if (listaNomes.includes(nome)) {
+    alert("Nome repetido, insira outro!");
   } else {
-    listaNomes.push(nome.value);
-    nome.value = "";
+    listaNomes.push(nome);
+    campoNome.value = "";
+    atualizarLista();
+  }
+}
+
+// Função para atualizar a lista de nomes na tela
+function atualizarLista() {
+  let lista = document.querySelector("#listaAmigos");
+  lista.innerHTML = "";
+  for (var i = 0; i < listaNomes.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = listaNomes[i];
+    lista.appendChild(li);
   }
 }
